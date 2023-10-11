8ff51798121780878c6fa42abcd3bd409883097b
yash heda <yashheda5@outlook.com>
2023-10-11 11:50:28 +0530

    adding work

--- 5_Searching/11_FindOddOccuringElement.cpp
+++ 5_Searching/11_FindOddOccuringElement.cpp
@@ -1,22 +1,45 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include<vector>
 using namespace std;
-int search(vector<int>arr){
-    int s=0;
-    int e=arr.size()-1;
-   
-    while(s<=e){
-        int mid =s+(e-s)/2;
-        if(s==e){
-            return s;
+
+int solve(vector<int> arr) {
+  int s = 0;
+  int e = arr.size() - 1;
+  int mid = s + (e-s)/2;
+
+  while(s <= e) {
+    if(s == e) {
+      //single element
+      return s;
+    }
+
+    //2 cases -> mid - even or mid - odd
+    if(mid%2 == 0 ) {
+      if(arr[mid] == arr[mid + 1] ) {
+        s = mid + 2;
+      }
+      else {
+        e = mid;
+      }
+    }
+    else {
+        if(arr[mid] == arr[mid-1]) {
+          s = mid + 1;
         }
-        if(arr[mid]= arr[mid+1]){
-            s=mid+2;
-        } else{
-            e=mid;
+        else {
+          e = mid - 1;
         }
     }
+    mid = s + (e-s)/2;
+  }
+  return -1;
 }
-int main(){
-    vector<int>arr{1,1,2,2,3,3,4,4,3,5,5,600,600};
-    cout<<search(arr);
+
+int main() {
+  vector<int> arr{1,1,2,2,1};
+  int ans = solve(arr);
+  cout << "index is " << ans << endl;
+  cout << "value is " << arr[ans] << endl;
+
+  return 0;
 }
---
+++ 5_Searching/12_PivotElement.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int pivot(vector<int> arr){
+    int s=0 , e=arr.size()-1;
+    while(s<=e){
+        if(s==e){
+            return s;
+        }
+        int mid =s+(e-s)/2;
+        if(mid+1<=e && arr[mid]>arr[mid+1]){
+            return arr[mid];
+        }
+        if(mid-1>= 0 && arr[mid-1]>arr[mid]){
+            return arr[mid-1];
+        }
+        if(arr[s]>arr[mid]){
+            e=mid-1;
+        }else{
+            s=mid+1;
+        }
+    }
+    return -1;
+}
+int main(){
+    vector<int>arr{9,10,1,2,3,4,5};
+    cout<<pivot(arr)<<endl;
+}
